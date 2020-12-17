6fec6879fe8b901a90b9948ba3eb2ea671925e04
Justin Liu on linux <justinliu4234@gmail.com>
2020-12-17 12:55:33 -0600

    getting odrive_test back ,need to adjust speed its too fast

--- scripts/odrive_test.py
+++ scripts/odrive_test.py
@@ -2,9 +2,10 @@
 
 import rospy
 import std_msgs.msg as m
-import math
 import modern_robotics as mr
 import odrive
+import time
+import numpy as np
 
 class DifferentialGearUnpack():
     # order of events:
@@ -26,8 +27,13 @@ class DifferentialGearUnpack():
         self._vel_T3 = 0.0
 
         self.speed_ratio = 3.95 # 20 to 89 teeth
-        self.rad_to_rev = 1 /(2*math.pi)
+        self.rad_to_rev = 1 /(2*np.pi)
         self.final_multipler = self.speed_ratio*self.rad_to_rev
+
+        self.previous_time_A = 0
+        self.previous_time_B = 0
+
+        self.minorSteps = np.pi/100
 
     @property
     def vel_R3(self):
@@ -56,8 +62,8 @@ class DifferentialGearUnpack():
     @vel_A.setter
     def vel_A(self, new_vel):
         if not mr.NearZero(self._vel_A - new_vel):
-            self._vel_A = new_vel
-            odrv0.axis0.controller.input_vel = self.final_multipler*self._vel_A
+            self._vel_A = self.final_multipler*new_vel
+            self.previous_time_A = time.perf_counter()
 
     @property
     def vel_B(self):
@@ -66,8 +72,8 @@ class DifferentialGearUnpack():
     @vel_B.setter
     def vel_B(self, new_vel):
         if not mr.NearZero(self._vel_B - new_vel):
-            self._vel_B = new_vel
-            odrv0.axis1.controller.input_vel = self.final_multipler*self._vel_B
+            self._vel_B = self.final_multipler*new_vel
+            self.previous_time_B = time.perf_counter()
 
     def updateMotorVel(self):
         self.vel_A = -1*self._vel_R3 + self._vel_T3
@@ -78,6 +84,19 @@ class DifferentialGearUnpack():
     
     def callbackT3(self, data):
         self.vel_T3 = data.data
+
+    def step(self):
+        if (self.vel_A != 0):
+            current_time = time.perf_counter()
+            if (current_time - self.previous_time_A) > (self.minorSteps/(self.vel_A+0.000001)):
+                odrv0.axis0.controller.input_pos += np.sign(self.vel_A)*self.minorSteps
+                self.previous_time_A = current_time
+
+        if (self.vel_B != 0):
+            current_time = time.perf_counter()
+            if (current_time - self.previous_time_B) > (self.minorSteps/(self.vel_B+0.000001)):
+                odrv0.axis1.controller.input_pos += np.sign(self.vel_B)*self.minorSteps
+                self.previous_time_B = current_time
 
 # need to add another topic in main_UI for home, and a subscriber in this file to catch it and home
 # look into startup sequence in odrive
@@ -91,7 +110,9 @@ def listener():
     rospy.Subscriber("chatter_motorT3", m.Float32, a.callbackT3)
     rospy.Subscriber("chatter_motorR3", m.Float32, a.callbackR3)
     rospy.loginfo("ready to read from chatter_motorT3/R3")
-    rospy.spin()
+    while True:
+        a.step()
+        #rospy.spinOnce()
 
 if __name__ == '__main__':
     listener()
