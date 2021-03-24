6ac8e1afeb5e78bd9dd155e7b7b7c374431cea63
from lubuntu <justinliu4234@gmail.com>
2021-03-23 21:33:53 -0500

    established coord system of d435i, have trajectory tester to confirm

--- scripts/tests/realsense/trajectory_tracking_test.py
+++ scripts/tests/realsense/trajectory_tracking_test.py
@@ -1,16 +1,3 @@
-## License: Apache 2.0. See LICENSE file in root directory.
-## Copyright(c) 2015-2017 Intel Corporation. All Rights Reserved.
-
-# create pipeline and config at start
-# pipeline_wrapper + pipeline_profile + device + device_product_line seems standard
-# set up config for both depth and color(width, height of pixel map, some scale, fps)
-# start pipeline
-# while True:
-#   get frames from pipeline.wait for frames
-#   get depth/color frames from frames.get_what_frame
-#   convert frame data into numpy array
-#   use openCV to alter image to be appealing
-
 import pyrealsense2 as rs
 import numpy as np
 import cv2
@@ -42,18 +29,16 @@ align = rs.align(align_to)
 
 depth_background = np.array([])
 
-# 10m/s
-# store all good contours in this list outside the try loop (try a deque)
-# associate an time to live with each contour, every loop decrease the time to live by 1
-# on each loop, look in sphere of influence around the good contour to see if any past contours are in the sphere
-#   sphere from x,y coords and depth
-#   use speed of 5m/s, little under 12 mph; with 30 fps, 0.03333 secs btwn frames --> sphere of 0.166m
-#   add current contour to all past contours in sphere,
-contour_storage = deque([deque([]),deque([]),deque([]),deque([]),deque([])])
+points = []
 
-# fig, ax = plt.subplots()
-# ax = fig.add_subplot(111, projection="3d")
-# ax.scatter(0, 0, 0)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
+ax.axes.set_xlim3d(left=-2, right=2)
+ax.axes.set_ylim3d(bottom=-2, top=2)
+ax.axes.set_zlim3d(bottom=0, top=2)
 
 try:
 
@@ -105,18 +90,15 @@ try:
 
         thresh, depth_mask = cv2.threshold(depth_cleaned,1,255,cv2.THRESH_BINARY_INV)
 
-        # https://stackoverflow.com/questions/41893029/opencv-canny-edge-detection-not-working-properly
         sigma = 0.33
         v = np.median(depth_cleaned_3d)
         lower = int(max(0, (1.0 - sigma) * v))
         upper = int(min(255, (1.0 + sigma) * v))    
-
         depth_canny = cv2.Canny(depth_cleaned_3d, lower, upper)
 
-        # https://stackoverflow.com/questions/60259169/how-to-group-nearby-contours-in-opencv-python-zebra-crossing-detection
-        # https://www.geeksforgeeks.org/find-and-draw-contours-using-opencv-python/
         contours, hierarchy = cv2.findContours(depth_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+        current_frame_points = []
         for c in contours:
             try:
                 ellipse = cv2.minEnclosingCircle(c)
@@ -143,29 +125,13 @@ try:
             point = rs.rs2_deproject_pixel_to_point(depth_intrin, [x,y], depth_frame.get_distance(int(x),int(y)))
             if (point[0]*point[1]*point[2] == 0):
                 continue
-            # ax.scatter(point[0], point[1], point[2])
-            # plt.pause(0.01)
-
-            linked = False
-
-            # check all past contours and see spheres of influence
-            for i in range(len(contour_storage)-1):
-                for past_point in contour_storage[i]:
-                    if ((past_point[0][0] - point[0])**2 + (past_point[0][1] - point[1])**2 + (past_point[0][2] - point[2])**2)**0.5 < 0.166*(4-i):
-                        past_point.appendleft(point)
-                        linked = True
-                        if len(past_point) > 5:
-                            print(past_point)
-
-
-            # # if no contours link, then add the contour in its own deque in the 5th deque in contour_storage
-            if not linked: 
-                contour_storage[4].append(deque([point]))
+            # current_frame_points.append(point)
+            points.append(point)
 
             # cv2.ellipse(depth_image_converted_3d, ellipse, (0,255,0),2)
-            cv2.circle(depth_cleaned_3d, (int(x),int(y)), int(radius), (0,255,0),2)
-
-        # images = np.hstack((depth_image_converted,depth_image_canny))        
+            cv2.circle(depth_cleaned_3d, (int(x),int(y)), int(radius), (0,255,0),2)  
+        # if current_frame_points:
+        #     points.append(current_frame_points)
 
         # Show images
         cv2.namedWindow('Main', cv2.WINDOW_AUTOSIZE)
@@ -173,13 +139,17 @@ try:
 
         cv2.namedWindow('Canny', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('Canny', depth_canny)
-        cv2.waitKey(1)
+        key = cv2.waitKey(1)
 
-        # empty 1st deque in contour_storage (ttl = 0)
-        contour_storage[0].clear()
-
-        # rotate contour storage left
-        contour_storage.rotate(-1)
+        if key & 0xFF == ord('q') or key == 27:
+            cv2.destroyAllWindows()
+            for i in range(len(points)):
+                ax.scatter(points[i][0], points[i][1], points[i][2])
+                ax.text(points[i][0], points[i][1], points[i][2], str(i))
+                plt.pause(0.01)
+            cv2.waitKey(10000)
+            
+            break
 
     plt.show()
 
