98be6e867751b09bc0fdc18e198b7b54c0719451
Justin on windows <justinliu4234@gmail.com>
2021-04-04 00:45:04 -0500

    good values saved on phone

--- scripts/tests/realsense/removing_shadows_test.py
+++ scripts/tests/realsense/removing_shadows_test.py
@@ -6,22 +6,54 @@ import cv2
 
 depth_scale = 0.0010000000474974513
 
-# Convert images to numpy arrays
-depth_image = np.load("scripts/tests/realsense/frames/depth_frame_0_15.npy")
-color_image = np.load("scripts/tests/realsense/frames/color_frame_0_15.npy")
+def nothing(x):
+    pass
+
+cv2.namedWindow('canny_cleaned', cv2.WINDOW_AUTOSIZE)
+
+depth_image = np.load("scripts/tests/realsense/frames/depth_frame_0_2.npy")
+color_image = np.load("scripts/tests/realsense/frames/color_frame_0_2.npy")
 depth_background = np.load("scripts/tests/realsense/frames/depth_frame_background.npy")
+
+cv2.createTrackbar("image", "canny_cleaned", 0,39, nothing)
+cv2.createTrackbar("sigma", "canny_cleaned", 0, 100, nothing)
+cv2.createTrackbar("bilateral_diameter", "canny_cleaned", 1,50, nothing)
+cv2.createTrackbar("bilateral_colorspace", "canny_cleaned", 1,100, nothing)
+cv2.createTrackbar("bilateral_coordspace", "canny_cleaned", 1,100, nothing)
 
 depth_cleaned = (depth_image*(255/(6/depth_scale))).astype(np.uint8)
 depth_cleaned = np.where((depth_cleaned > 255), 255, depth_cleaned)
 depth_cleaned = np.where((depth_cleaned <= 0), depth_background, depth_cleaned)
 depth_cleaned = cv2.bilateralFilter(depth_cleaned, 9, 50, 50)
+
 depth_cleaned_3d = np.dstack((depth_cleaned,depth_cleaned,depth_cleaned))
+sigma = 0.33
+v = np.median(depth_cleaned_3d)
+lower = int(max(0, (1.0 - sigma) * v))
+upper = int(min(255, (1.0 + sigma) * v))    
 
-cv2.namedWindow('canny_cleaned', cv2.WINDOW_AUTOSIZE)
+depth_canny = cv2.Canny(depth_cleaned, lower, upper)
+depth_canny_3d = np.dstack((depth_canny, depth_canny, depth_canny))
 
-def on_sigma_trackbar(new_sigma):
-    sigma = new_sigma/100
-    # sigma = 0.33
+while 1:
+    images = np.hstack((depth_canny_3d, depth_cleaned_3d))
+    cv2.imshow('canny_cleaned', images)
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
+        break
+
+    depth_image = np.load("scripts/tests/realsense/frames/depth_frame_0_" + str(cv2.getTrackbarPos("image", "canny_cleaned")) + ".npy")
+    color_image = np.load("scripts/tests/realsense/frames/color_frame_0_" + str(cv2.getTrackbarPos("image", "canny_cleaned")) + ".npy")
+    depth_cleaned = (depth_image*(255/(6/depth_scale))).astype(np.uint8)
+    depth_cleaned = np.where((depth_cleaned > 255), 255, depth_cleaned)
+    depth_cleaned = np.where((depth_cleaned <= 0), depth_background, depth_cleaned)
+    depth_cleaned = cv2.bilateralFilter(depth_cleaned, cv2.getTrackbarPos("bilateral_diameter", "canny_cleaned"),\
+                                                       cv2.getTrackbarPos("bilateral_colorspace", "canny_cleaned"),\
+                                                       cv2.getTrackbarPos("bilateral_coordspace", "canny_cleaned"))
+
+    depth_cleaned_3d = np.dstack((depth_cleaned,depth_cleaned,depth_cleaned))
+
+    sigma = cv2.getTrackbarPos("sigma", "canny_cleaned")/100
     v = np.median(depth_cleaned_3d)
     lower = int(max(0, (1.0 - sigma) * v))
     upper = int(min(255, (1.0 + sigma) * v))    
@@ -30,25 +62,16 @@ def on_sigma_trackbar(new_sigma):
     depth_canny_3d = np.dstack((depth_canny, depth_canny, depth_canny))
 
     contours, hierarchy = cv2.findContours(depth_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
-    print(hierarchy)
 
     for c in contours:
         try:
             (x,y), radius = cv2.minEnclosingCircle(c)              
         except: continue
         cv2.circle(depth_canny_3d, (int(x),int(y)), 3, (0,0,255), 1)
-        # removing weird edge cases
         if x*y*radius <= 0: continue
-        # # removing small contours
         contour_area = cv2.contourArea(c)
         if contour_area < 2: continue
 
         cv2.circle(depth_cleaned_3d, (int(x),int(y)), int(radius), (0,255,0),2)
 
-    images = np.hstack((depth_canny_3d, depth_cleaned_3d))
-    cv2.imshow('canny_cleaned', images)
-
-cv2.createTrackbar("sigma", "canny_cleaned", 0, 100, on_sigma_trackbar)
-on_sigma_trackbar(25)
-
-cv2.waitKey()
+cv2.destroyAllWindows()
