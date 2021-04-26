1e429170e5d55d236e0e0d809d3a21386ad1e107
Justin on windows <justinliu4234@gmail.com>
2021-04-25 23:51:51 -0500

    bounds of workspace without considering R3T3 good

---
+++ scripts/constants/coarse_workspace.py
@@ -0,0 +1,9 @@
+# for the inner bound
+# real radius is 0.098989m from T1, add in 0.025m for the R3T3 tilt extension
+# R1 angle is [-60,60]
+# T1 angle is [43.587, 223.587] 0 is parallel to ground level towards the back
+
+# for outer bound
+# real radius is 0.4337037m from T1, sub 0.025m for R3T3 til extension
+# R1 angle is [-60,60]
+# T1 angle is [-9.054, 170.946]
