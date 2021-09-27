93d0d5ce740599a96a4b3c9ee27e885921114329
Justin on windows <justinliu4234@gmail.com>
2021-09-26 23:04:01 -0400

    plotting NR to see if it grabs the right root

--- scripts/modules/Trajectory.py
+++ scripts/modules/Trajectory.py
@@ -306,6 +306,8 @@ class Trajectory():
         # using a starting guess of 0 is close to a flat slope, which is really bad for NR
         possible, collision_time, new_estimates = self.newtonRaphsonQuartic(a, self.times[-1], 0.001)
 
+        self.plotNewtonRaphson(collision_time, new_estimates)
+        
         if possible:
             # check if the collision_time is feasible to get to, but need MC stuff: have to get out      
             point = [self.getCoordFromTime("x", collision_time), \
